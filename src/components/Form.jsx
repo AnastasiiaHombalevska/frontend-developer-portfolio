7b8191dea716f69361d8da43bfd853924bcdd4af
Anastasiia <vasilisandreeva00@gmail.com>
2024-09-12 12:35:29 +0300

    add form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,43 @@
 import React from "react";
 
+function updateName(name) {
+  if (!name) {
+    return;
+  } else {
+    return name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+  }
+}
+
 export default function Form() {
+  function sendMsg(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const message = formData.get("message");
+
+    if (!name || !email || !message) {
+      return;
+    } else {
+      alert(`${updateName(name)}, your message has been successfully sent!`);
+      console.log(name, email, message);
+      e.target.reset();
+    }
+  }
+
   return (
-    <form className="contacts--form form">
-      <label htmlFor="">
-        <input type="text" placeholder="Name" className="form--field form--input"/>
+    <form className="contacts--form form" onSubmit={sendMsg}>
+      <label>Name:
+        <input type="text" name="name" className="form--field form--input"/>
       </label>
 
-      <label htmlFor="">
-        <input type="email" placeholder="Email" className="form--field form--input"/>
+      <label>Email:
+        <input type="email" name="email" className="form--field form--input"/>
       </label>
 
-      <label htmlFor="">
-        <textarea name="" id="" cols="30" rows="10" className="form--field form--textarea">Message</textarea>
+      <label>Message:
+        <textarea name="message" cols="30" rows="10" className="form--field form--textarea"></textarea>
       </label>
 
       <button type="submit" className="form--btn">Send message</button>
