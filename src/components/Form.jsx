970fdc187a9e8397b0f79d4dedf8017fe7040c84
Anastasiia <vasilisandreeva00@gmail.com>
2024-09-12 13:32:43 +0300

    add message for err in the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-function updateName(name) {
-  if (!name) {
-    return;
-  } else {
-    return name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
-  }
-}
+import FormMessage from "./FormMessage";
 
 export default function Form() {
+  const [messageState, setMessageState] = useState(null);
+  const [userName, setUserName] = useState("");
+
   function sendMsg(e) {
     e.preventDefault();
 
@@ -18,29 +15,31 @@ export default function Form() {
     const message = formData.get("message");
 
     if (!name || !email || !message) {
+      setMessageState(false)
       return;
     } else {
-      alert(`${updateName(name)}, your message has been successfully sent!`);
-      console.log(name, email, message);
       e.target.reset();
+      setUserName(name);
+      setMessageState(true);
     }
   }
 
   return (
-    <form className="contacts--form form" onSubmit={sendMsg}>
-      <label>Name:
-        <input type="text" name="name" className="form--field form--input"/>
-      </label>
+      <form className="contacts--form form" onSubmit={sendMsg}>
+        <label>Name:
+          <input type="text" name="name" className="form--field form--input"/>
+        </label>
 
-      <label>Email:
-        <input type="email" name="email" className="form--field form--input"/>
-      </label>
+        <label>Email:
+          <input type="email" name="email" className="form--field form--input"/>
+        </label>
 
-      <label>Message:
-        <textarea name="message" cols="30" rows="10" className="form--field form--textarea"></textarea>
-      </label>
+        <label>Message:
+          <textarea name="message" cols="30" rows="10" className="form--field form--textarea"></textarea>
+        </label>
 
-      <button type="submit" className="form--btn">Send message</button>
+        <button type="submit" className="form--btn">Send message</button>
+      {messageState !== null && <FormMessage state={messageState} userName={userName} />}
     </form>
   )
 }
---
+++ src/components/FormMessage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function updateName(name) {
+  if (!name) {
+    return;
+  } else {
+    return name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+  }
+}
+
+export default function FormMessage({ state, userName }) {
+  console.log('FormMessage props:', { state, userName });
+
+  const success = `${updateName(userName)}, your message has been successfully sent!`;
+
+  const err = "Are all the input data filled in?";
+
+  return (
+    <div className = "formMessage" >
+    {
+        state ? (
+          <>
+             <h4 className="formMessage--title">Nice!</h4>
+             <p className="formMessage--msg">{success}</p>
+          </>
+        ) : (
+          <>
+            <h4 className="formMessage--title err">Something went wrong!</h4>
+            <p className="formMessage--msg">{err}</p>
+          </>
+          )
+      }
+    </div>
+  )
+}
