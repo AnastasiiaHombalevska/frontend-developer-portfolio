7b31ffd788b88a063f9abdfc10502e99c5290d69
Anastasiia <vasilisandreeva00@gmail.com>
2024-09-20 18:36:12 +0300

    add nav tablet version

--- src/components/DesktopNav.jsx
+++ src/components/DesktopNav.jsx
@@ -3,12 +3,18 @@ import React from "react";
 export default function DesktopNav() {
   return (
     <nav className="nav">
-      <ul className="nav--list">
-          <li><a href="#portfolio" className="nav--link">Work</a></li>
-          <li><a href="#about" className="nav--link">About</a></li>
-          <li><a href="#skills" className="nav--link">Skills</a></li>
-          <li><a href="#contacts" className="nav--link">Contacts</a></li>
+      <div className="nav--container">
+        <a href="#menu" className="nav--link logo logo--author"></a>
+
+        <ul className="nav--list">
+            <li><a href="#portfolio" className="nav--link">Work</a></li>
+            <li><a href="#about" className="nav--link">About</a></li>
+            <li><a href="#skills" className="nav--link">Skills</a></li>
+            <li><a href="#contacts" className="nav--link">Contacts</a></li>
         </ul>
+
+        <button className="nav--btn">Get resume</button>
+      </div>
     </nav>
   )
 }
