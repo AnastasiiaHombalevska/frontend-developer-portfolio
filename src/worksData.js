1c3de962e473f1db576744d30da638fa9520bb6e
Anastasiia <vasilisandreeva00@gmail.com>
2024-09-24 09:32:27 +0300

    add link hovers

---
+++ src/worksData.js
@@ -0,0 +1,63 @@
+import scrUnitConversion from "./images/portfolio-screenshots/metric-Imperial-unit-conversion.png";
+import scrPasswordGenerator from "./images/portfolio-screenshots/random-password-generator.png";
+import scr2DGame from "./images/portfolio-screenshots/2D-game.png";
+import scrCreativeBakery from "./images/portfolio-screenshots/creative-bakery.png";
+import scrMuseum from "./images/portfolio-screenshots/museum.png";
+import scrMuseum2 from "./images/portfolio-screenshots/museum2-0.png";
+
+export default [
+  {
+    number: 1,
+    name: "Metric/Imperial Unit Conversion",
+    imgSourse: scrUnitConversion,
+    description: "screenshoot of metric/Imperial Unit Conversion",
+    skills: ["HTML", "CSS", "JavaScript"],
+    links: [{ "View live": "https://brilliant-cajeta-6a28eb.netlify.app/" }, { "View github": "https://github.com/AnastasiiaHombalevska/unit-converter" }]
+  },
+
+  {
+    number: 2,
+    name: "Random password generator",
+    imgSourse: scrPasswordGenerator,
+    description: "screenshoot of random password generator",
+    skills: ["HTML", "CSS", "JavaScript"],
+    links: [{ "View live": "https://random-password-withjs.netlify.app/" }, { "View github": "https://github.com/AnastasiiaHombalevska/random-password-generator" }]
+  
+  },
+
+  {
+    number: 3,
+    name: "CV 2D-game with JavaScript",
+    imgSourse: scr2DGame,
+    description: "screenshoot of CV 2D-game with JavaScript",
+    skills: ["HTML", "CSS", "JavaScript"],
+    links: [{ "View live": "https://anastasiiahombalevska.github.io/AnastasiiaHombalevska2D_portfolio_game-/" }, { "View github": "https://github.com/AnastasiiaHombalevska/AnastasiiaHombalevska2D_portfolio_game-" }]
+  },
+
+  {
+    number: 4,
+    name: "Museum 2.0",
+    imgSourse: scrMuseum2,
+    description: "landing page screenshoot of museum 2.0",
+    skills: ["HTML", "SASS(SCSS)"],
+    links: [{ "View live": "https://anastasiiahombalevska.github.io/art_museum2.0/" }, { "View github": "https://github.com/AnastasiiaHombalevska/art_museum2.0" }]
+  },
+
+  {
+    number: 5,
+    name: "Creative Bakery",
+    imgSourse: scrCreativeBakery,
+    description: "landing page screenshoot of Creative Bakery",
+    skills: ["HTML", "SASS(SCSS)"],
+    links: [{ "View live": "https://anastasiiahombalevska.github.io/Creative_Bakery/" }, { "View github": "https://github.com/AnastasiiaHombalevska/Creative_Bakery" }]
+  },
+
+  {
+    number: 6,
+    name: "Museum",
+    imgSourse: scrMuseum,
+    description: "landing page screenshoot of museum",
+    skills: ["HTML", "SASS(SCSS)"],
+    links: [{ "View live": "https://anastasiiahombalevska.github.io/art_museum/" }, { "View github": "https://github.com/AnastasiiaHombalevska/art_museum" }]
+  }
+]
