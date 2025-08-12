3190514540bb7f6fa78a1e491d63efd61004bb3c
Anastasiia Hombalevska <87438694+AnastasiiaHombalevska@users.noreply.github.com>
2025-08-12 11:10:07 +0300

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const RESUME_PATH = "https://drive.google.com/file/d/1Z9GPogp6Aql6HZCj9NcDCPERnlb5jz-r/view?usp=sharing";
+const RESUME_PATH = "https://drive.google.com/file/d/1aL6J1axG3X0mkCk6CVk6pD2s7dMKa-mq/view?usp=sharing";
 
 export default function ResumeLink(props) {
   const { modificator } = props;
 
   return (
-    <a href={RESUME_PATH} target="_blank">
+    <a href={RESUME_PATH} target="_blank" rel="noopener noreferrer">
       <button className={`resume_link--btn ${modificator}`}>View resume</button>
     </a>
   )
