0b35ffb40d7f253e008d03cd301e8c7a9506faad
Anastasiia <vasilisandreeva00@gmail.com>
2026-04-17 13:06:05 +0300

    update err msg

--- src/components/FormMessage.jsx
+++ src/components/FormMessage.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 
-export default function FormMessage({ state, userName }) {
-  function updateName(name) {
-    if (!name) {
-      return;
-    } else {
-      return name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
-    }
-  }
-
-  const success = `${updateName(userName)}, your message has been successfully sent!`;
-  const err = 'Have you filled in all the fields?';
+export default function FormMessage({ state }) {
+  const success = 'Your message has been successfully sent!';
+  const err = 'Have you filled all the fields?';
 
   return state ? (
     <div className="formMessage success">
